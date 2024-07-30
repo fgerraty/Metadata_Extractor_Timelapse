4511bb970d343458ff39728e330154ba952bcad4
Frankie Gerraty <frankiegerraty@gmail.com>
2024-07-29 17:40:51 -0700

    scratch paper

--- scripts/scratch.R
+++ scripts/scratch.R
@@ -82,6 +82,13 @@ cleaned_string <- str_replace_all(example_string, "(\\d)83", "\\18")
 print(cleaned_string)
 
 
+all_results <- arrange(all_results, date_time_parsed)
+
+print(all_results[75:90, 3:6])
+
+print(files_to_check[1:4, 1:3])
+
+
 
 
 # Create a Tesseract engine with options to recognize specific characters and treat the input as a single line of text
@@ -90,7 +97,8 @@ date_time_engine <- tesseract("eng", options = list(
   tessedit_pageseg_mode = '7'  # Assume a single text line
 ))
 
-photo  <- magick::image_read("data/extracted_images/sequence2/CCAM12_D3_11.jpg") # import image
+photo  <- magick::image_read("data/extracted_images/sequence2/CCAM12_D3_10.jpg") # import image
+photo
 
 date_time_crop <- magick::image_crop(image = photo, geometry = "350x40+2330+1480")
 
@@ -98,3 +106,26 @@ date_time_crop
 
 date_time <- tesseract::ocr_data(date_time_crop, engine = date_time_engine)
 date_time
+
+
+
+
+
+
+
+
+
+
+
+
+out_of_order <- all_results %>% 
+  mutate(
+    ccam_num = str_extract(file_name, "(?<=CCAM)\\d+"),
+    day_num = str_extract(file_name, "(?<=D)\\d+"),
+    photo_num = str_extract(file_name, "(?<=_)\\d+(?=\\.jpg)")) %>% 
+  mutate(photo_num = as.numeric(photo_num)) %>% # Convert photo_num to numeric
+  arrange(ccam_num, day_num, photo_num) %>% 
+  group_by(ccam_num, day_num) %>%
+  mutate(is_out_of_order = date_time_parsed < lag(date_time_parsed, default = first(date_time_parsed))) %>%
+  ungroup()
+
