4aa54b6fce55ab3456b9b9888f4259e4422a3d1c
Frankie Gerraty <frankiegerraty@gmail.com>
2024-07-29 17:40:13 -0700

    Add double-checking steps and corrections.

--- scripts/Metadata_Extraction.R
+++ scripts/Metadata_Extraction.R
@@ -93,10 +93,74 @@ process_sequence <- function(sequence_dir) {
 sequence_dirs <- list.dirs("data/extracted_images", full.names = TRUE, recursive = FALSE)
 
 # Apply the process_sequence function to all sequence directories and combine the results into a single dataframe
-all_results <- map_dfr(sequence_dirs, process_sequence)
+all_results <- map_dfr(sequence_dirs, process_sequence) 
 
 # Print the results
 print(all_results)
 
 
 #As you can see, there are a few errors that we will have to manually correct, but most of the data was extracted successfully! 
+
+
+####################################################
+# Part 3: Manual Correction of Errors ##############
+####################################################
+
+#The process I outlined above is far from perfect, and I have built a few double-checking steps into my workflow to correct for the errors that arise. First, lets correct the values that failed when parsing to date-time. This typically occurs when tesseract OCR does not interpret the image correctly. 
+
+# Identify rows where parsing failed
+failed_rows <- all_results %>% filter(is.na(date_time_parsed))
+
+# Manually corrected date-time values
+corrected_date_times <- c(
+  "07/06/2023 11:15:06 AM",
+  "07/06/2023 11:18:03 AM",
+  "10/05/2023 08:18:37 AM"
+  # Add more corrected values as needed
+)
+
+# Assign corrected values back to the dataframe
+all_results <- all_results %>%
+  mutate(date_time = replace(date_time, which(is.na(date_time_parsed)), corrected_date_times)) %>% 
+  # Re-parse the corrected date-time values
+  mutate(date_time_parsed = parse_date_time(date_time, "mdY HMS p"))
+
+
+#Next, we need to screen for more sneaky errors that sneak in due to errors in the tesseract OCR character recognition system. Tesseract seems to have trouble mixing up 3s and 8s, as well as 1s and 7s. To flag errors, I first pull data that I have build into the photo file names (ccam_num is the unique carcass ID, day_num identifies the timelapse day of carcass monitoring, and phot_num is the unique photo number) and then double check that the date-time is in the correct order for the photo sequence
+
+out_of_order <- all_results %>% 
+  mutate(
+    ccam_num = str_extract(file_name, "(?<=CCAM)\\d+"),
+    day_num = str_extract(file_name, "(?<=D)\\d+"),
+    photo_num = str_extract(file_name, "(?<=_)\\d+(?=\\.jpg)")) %>% 
+  mutate(photo_num = as.numeric(photo_num)) %>% # Convert photo_num to numeric
+  arrange(ccam_num, day_num, photo_num) %>% 
+  group_by(ccam_num, day_num) %>%
+  mutate(is_out_of_order = date_time_parsed < lag(date_time_parsed, default = first(date_time_parsed))) %>%
+  ungroup()
+
+#As you can see, there are four errors thrown here (value in column is_out_of_order = TRUE) that are much more stealthy than the prior "failure to parse" error. Fixing these errors usually involve checking the original images that throw an error AND adjacent photos. These are the files to check:
+
+files_to_check <- out_of_order %>%
+  filter(is_out_of_order) 
+
+#In this case, we flagged four additional files that were incorrect (and they were not all the ones identified in the files_to_check dataframe...the real error associated with the file CCAM6_D1_72.jpg was actually a mis-read in the previous image file CCAM6_D1_71.jpg)
+
+out_of_order_corrections <- tibble(
+  file_name = c("CCAM12_D3_10.jpg", "CCAM12_D3_13.jpg", 
+                "CCAM12_D3_42.jpg", "CCAM6_D1_71.jpg"),
+  date_time = c("10/05/2023 08:08:36 AM", "10/05/2023 08:38:37 AM", 
+                "10/05/2023 11:48:37 AM", "07/06/2023 05:21:05 PM"))
+
+# Join the dataframes
+all_results <- left_join(all_results, out_of_order_corrections, by = "file_name", suffix = c("", "_corrected")) %>% 
+  #Override incorrect date time with corrected one, where applicable
+  mutate(date_time = if_else(!is.na(date_time_corrected), date_time_corrected, date_time),
+         #re-parse date-time
+         date_time_parsed = parse_date_time(date_time, "mdY HMS p")) %>%
+  select(-date_time_corrected)
+
+#Together, these double-checking steps help to minimize errors that are introduced in the optical character recognition process. Let's save the all_results dataframe as a .csv for future use. 
+
+write_csv(all_results, "output/all_results.csv")
+
